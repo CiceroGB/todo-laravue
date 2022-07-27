f38a864b14a11566a5521f757165f27ea53c697d
Cicero Gasparini Brunelli <brunelli.cg@gmail.com>
2022-07-27 14:04:32 -0300

    chore: add vue vuex

--- app/src/main.js
+++ app/src/main.js
@@ -4,11 +4,13 @@ import App from './App.vue'
 
 import '@/assets/css/tailwind.css';
 import router from './router'
+import store from './store'
 
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
+  store,
   render: h => h(App)
 }).$mount('#app')
