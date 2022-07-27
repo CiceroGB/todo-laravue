4ee4fbe391a09dc702563a25bb490f260a955191
Cicero Gasparini Brunelli <brunelli.cg@gmail.com>
2022-07-27 14:17:33 -0300

    fix: plugin vee-validate

--- app/src/main.js
+++ app/src/main.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+import store from './store'
+import '@/plugins/vee-validate';
 
 
 import '@/assets/css/tailwind.css';
-import router from './router'
-import store from './store'
+
 
 
 Vue.config.productionTip = false
